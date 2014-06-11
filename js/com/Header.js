bea9d8765c3452139d6ff991fcbc60c60ca2b509
Mdinteractivo <mdinteractivo@gmail.com>
2014-06-11 17:22:55 +0200

    V123

--- js/com/Header.js
+++ js/com/Header.js
@@ -7,27 +7,21 @@
 		self.div = document.createElement('div');
 		self.div.className = 'header-seccion';
 
-		var logofifa = new Image();
-			logofifa.src = 'img/general/logo-fifa.png';
-			logofifa.id = 'logo-fifa';
-			logofifa.width = 107;
-			logofifa.height = 106;
-			$(self.div).append(logofifa);
-		
-		var holderUserName = document.createElement('div');
-			holderUserName.id = 'holder-user-name';
-			$(self.div).append(holderUserName);
-		
 		var logo = new Image();
 			logo.src = 'img/general/logo-pacena.png';
 			logo.id = 'logo-pacena';
 			logo.width = 92;
 			logo.height = 77;
 			$(self.div).append(logo);
+		
+		var holderUserName = document.createElement('div');
+			holderUserName.id = 'holder-user-name';
+			$(self.div).append(holderUserName);
+		
 			
 		self.animIn = function()
 		{
-			$(logofifa).delay(0).css({scale : 0.5, y : -110}).transition({duration : 400, scale : 1, y : 0});
+			//$(logofifa).delay(0).css({scale : 0.5, y : -110}).transition({duration : 400, scale : 1, y : 0});
 			$(holderUserName).delay(300).css({opacity : 0}).transition({opacity : 1});
 			$(logo).delay(0).css({scale : 0.5, rotate : -210, opacity : 0}).transition
 			({
